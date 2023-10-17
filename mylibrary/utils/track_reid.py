a15600c8c0ae24a2202e7031771900881aff360d
Preference-Kim <preferencekimm@gmail.com>
2023-10-17 17:35:15 +0900

    Add TrackCamThread Class

---
+++ mylibrary/utils/track_reid.py
@@ -0,0 +1,132 @@
+import os
+from copy import deepcopy
+from threading import Thread
+
+import cv2
+import numpy as np
+import torch
+
+from . import bt_util
+from ..nets import nn as bt
+
+class TrackCamThread(Thread):
+    def __init__(self, model, idx, fps, sz, cap, frames_queue, conf=0.001, iou=0.1):
+        super(TrackCamThread, self).__init__()
+        self.model = model
+        self.idx = idx
+        self.fps = fps
+        self.sz = sz
+        self.cap = cap
+        self.frames_queue = frames_queue
+        self.conf = conf
+        self.iou = iou
+        self.bt = bt.BYTETracker(self.fps)
+        self.count = -1
+        ###
+        self.frame_ant = None
+        self.record = True
+        self.stride = 3
+    
+    def run(self):
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            else:
+                outputs = self.track(frame)
+                self.frame_ant = frame.copy()
+                if len(outputs) > 0:
+                    boxes = outputs[:, :4]
+                    identities = outputs[:, 4]
+                    object_classes = outputs[:, 6]
+                    for i, box in enumerate(boxes):
+                        if object_classes[i] != 0:  # 0 is for person class (COCO)
+                            continue
+                        x1, y1, x2, y2 = list(map(int, box))
+                        # get ID of object
+                        index = int(identities[i]) if identities is not None else 0
+                        if self.count%self.stride == 0 and self.record:
+                            crop_dir = f'images/buf/{self.idx}'
+                            os.makedirs(crop_dir, exist_ok=True)
+                            cropped = deepcopy(frame[y1:y2, x1:x2])
+                            if cropped is not None and cropped.size > 0:
+                                cv2.imwrite(f'{crop_dir}/id{index}_{self.count}.jpg', cropped)
+                        draw_line(self.frame_ant, x1, y1, x2, y2, index)
+            
+            self.frames_queue.put(self.frame_ant) # Send the frame to the main thread for displaying
+    
+    def track(self,frame):
+        self.count += 1
+        
+        boxes = []
+        confidences = []
+        object_classes = []
+
+        image = frame.copy()
+        shape = image.shape[:2]
+
+        r = self.sz / max(shape[0], shape[1])
+        if r != 1:
+            h, w = shape
+            image = cv2.resize(image,
+                            dsize=(int(w * r), int(h * r)),
+                            interpolation=cv2.INTER_LINEAR)
+
+        h, w = image.shape[:2]
+        image, ratio, pad = bt_util.resize(image, self.sz)
+        shapes = shape, ((h / shape[0], w / shape[1]), pad)
+        # Convert HWC to CHW, BGR to RGB
+        sample = image.transpose((2, 0, 1))[::-1]
+        sample = np.ascontiguousarray(sample)
+        sample = torch.unsqueeze(torch.from_numpy(sample), dim=0)
+
+        sample = sample.cuda()
+        sample = sample.half()  # uint8 to fp16/32
+        sample = sample / 255  # 0 - 255 to 0.0 - 1.0
+
+        # Inference
+        with torch.no_grad():
+            outputs = self.model(sample)
+
+        # NMS
+        outputs = bt_util.non_max_suppression(outputs, self.conf, self.iou) #outputs, conf_threshold=0.25, iou_threshold=0.45
+        for i, output in enumerate(outputs):
+            detections = output.clone()
+            bt_util.scale(detections[:, :4], sample[i].shape[1:], shapes[0], shapes[1])
+            detections = detections.cpu().numpy()
+            for detection in detections:
+                x1, y1, x2, y2 = list(map(int, detection[:4]))
+                boxes.append([x1, y1, x2, y2])
+                confidences.append(detection[4])
+                object_classes.append(detection[5])
+        outputs = self.bt.update(np.array(boxes),
+                                np.array(confidences),
+                                np.array(object_classes))
+        return outputs
+        
+def draw_line(image, x1, y1, x2, y2, index):
+    w = 10
+    h = 10
+    color = (200, 0, 0)
+    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 200, 0), 2)
+    # Top left corner
+    cv2.line(image, (x1, y1), (x1 + w, y1), color, 3)
+    cv2.line(image, (x1, y1), (x1, y1 + h), color, 3)
+
+    # Top right corner
+    cv2.line(image, (x2, y1), (x2 - w, y1), color, 3)
+    cv2.line(image, (x2, y1), (x2, y1 + h), color, 3)
+
+    # Bottom right corner
+    cv2.line(image, (x2, y2), (x2 - w, y2), color, 3)
+    cv2.line(image, (x2, y2), (x2, y2 - h), color, 3)
+
+    # Bottom left corner
+    cv2.line(image, (x1, y2), (x1 + w, y2), color, 3)
+    cv2.line(image, (x1, y2), (x1, y2 - h), color, 3)
+
+    text = f'ID:{str(index)}'
+    cv2.putText(image, text,
+                (x1, y1 - 2),
+                0, 1 / 2, (0, 255, 0),
+                thickness=1, lineType=cv2.FILLED)
--- project/track-reid.py
+++ project/track-reid.py
@@ -12,21 +12,29 @@ import sys
 sys.path.append("/home/sunhokim/Documents/mygit/TrackAndRe-ID")
 sys.path.append("/home/sunhokim/Documents/mygit/TrackAndRe-ID/mylibrary")
 
-from threading import Thread, active_count
+from threading import active_count
 import queue 
 
 import cv2
 import numpy as np
 import torch
+import torch.nn as nn
 
-from mylibrary import track_cam
+import torchreid
 from mylibrary.utils.loader import LoadStreams  # Import the LoadStreams class from the specified module
-from mylibrary.utils import bt_util, LOGGER
-from mylibrary.nets import nn as bt
+from mylibrary.utils import LOGGER
+from mylibrary.utils.track_reid import TrackCamThread
 
-model = torch.load('/home/sunhokim/Documents/mygit/TrackAndRe-ID/weights/yolo/v8_n.pt', map_location='cuda')['model'].float()
+model = torch.load('weights/yolo/v8_s.pt', map_location='cuda')['model'].float()
 model.eval()
 model.half()
+
+# model_reid = torchreid.models.build_model(
+#         name="osnet_ain_x1_0",
+#         num_classes=1000,
+#         pretrained = False
+#     )
+# torchreid.utils.load_pretrained_weights(model=model_reid, weight_path="weights/reid/imageNet-market.pth.tar-60")
 
 # List of multiple RTSP sources
 rtsp_sources = [
@@ -39,8 +47,6 @@ rtsp_sources = [
 # Initialize the LoadStreams class
 streams = LoadStreams(sources=rtsp_sources, imgsz=640, buffer=True)
 
-print('streams.imgsz', streams.imgsz)
-
 # Create a queue for frames to be displayed in the main thread
 frames_queue = [queue.Queue() for _ in rtsp_sources]
 
@@ -49,13 +55,16 @@ capture_gen = streams.cap_gen()
 
 # Create and start threads for each video source
 threads = []
+exit_flags = [False] * len(rtsp_sources)
+
 for i, cap in enumerate(capture_gen):
-    thread = Thread(target=track_cam, args=(model, i, streams.fps[i], streams.imgsz, cap, frames_queue, 0.2, 0.55))
+    thread = TrackCamThread(model, i, streams.fps[i], streams.imgsz, cap, frames_queue[i], 0.35, 0.7)
+    thread.daemon = True
     LOGGER.info(f'Video Thread ~~~~~~~~~~~~~~~~~~~~~ #{i}')
     threads.append(thread)
     thread.start()
 
-print(f"현재 실행 중인 쓰레드 수: {active_count()}")
+print(f"current number of running threads: {active_count()}")
 
 # Create a main thread for displaying frames
 while True:
@@ -63,15 +72,14 @@ while True:
 
     for i, frame in enumerate(frames):
         window_name = f"RTSP Stream {i}"
-        cv2.imshow(window_name, frame.astype('uint8'))
+        if frame is not None:
+            cv2.imshow(window_name, frame.astype('uint8'))
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        for i in range(len(threads)):
+            exit_flags[i] = True  # Set the thread termination conditions
         streams.close()
         cv2.destroyAllWindows()
         break
 
-print(f"현재 실행 중인 쓰레드 수: {active_count()}")
-
-# Wait for all threads to complete
-for thread in threads:
-    thread.join()
+print(f"current number of running threads: {active_count()}")
